b0de4f940892bfab888a6e231929cce845271679
Brunopiastre <b.piastrellini@alumno.um.edu.ar>
2025-10-23 14:30:04 -0300

    first commit

---
+++ KotlinPrimeraClase/composeApp/src/commonTest/kotlin/org/emicora/project/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package org.emicora.project
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
---
+++ KotlinPrimeraClase/composeApp/src/iosMain/kotlin/org/emicora/project/MainViewController.kt
@@ -0,0 +1,5 @@
+package org.emicora.project
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
